4e2a027d490788a78bcae40f71594ba81c91bb02
M2kura <136367855+M2kura@users.noreply.github.com>
2025-04-24 15:20:35 +0200

    Update autocmds.lua to use the newer vim.hl API

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -6,6 +6,6 @@ api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
   end,
 })
